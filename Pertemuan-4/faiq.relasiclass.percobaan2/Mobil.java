44042caedbc3f726446a11b677c4faae5b02aae2
faiqnabil <fq28nabil@gmail.com>
2025-09-28 13:47:58 +0700

    Add files via upload

---
+++ Pertemuan-4/faiq.relasiclass.percobaan2/Mobil.java
@@ -0,0 +1,29 @@
+public class Mobil {
+    private String merk;
+    private int biaya;
+
+    public Mobil() {
+        this.merk = "";
+        this.biaya = 0;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(int biaya) {
+        this.biaya = biaya;
+    }
+
+    public int hitungBiayaMobil(int hari) {
+        return biaya * hari;
+    }
+}
